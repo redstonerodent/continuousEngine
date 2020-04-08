6982f276fbf5369572a17b60e376a346839d5ee7
dylan <dylanhen@mit.edu>
2020-04-08 02:26:07 -0400

    made reversi significantly faster

--- reversi.py
+++ reversi.py
@@ -1,5 +1,7 @@
 from continuousEngine import *
 
+# Note to anyone reading this code: sometimes p (or p1 or p2 or pt or pv) is a pair of floats representing a point, and sometimes it's a ReversiPiece. I'm sorry.
+
 # needs to be more than 1/sqrt(6)~=0.408 for diagonals to work the same as in the discreet game
 piece_rad = .45
 
@@ -33,40 +35,45 @@ on_line_tangents = lambda p1, p2, x: (lambda d:
             (2*piece_rad*(p2[0]-p1[0])/dist_sq(p1,p2)**.5,2*piece_rad*(p2[1]-p1[1])/dist_sq(p1,p2)**.5) if d < 3 else
         ()
     )(dist_to_line(p1,p2,x)/piece_rad)
+
+# given locations of pieces [(float, float)] pts, the set of locations you could place a piece tangent to multiple existing pieces
+# near_pts is a dictionary {(float,flot): {(float,float)}} giving the set of pieces close enough to each piece to be relevant
+safe_tangents = lambda pts: (lambda near_pts:
+    {x for p in pts for q in near_pts[p] for x in double_tangents(p,q)
+        if not any(overlap(x,r) for r in near_pts[p] & near_pts[q])
+    }) ({p:{q for q in pts if dist_sq(p,q) < (4*piece_rad)**2} for p in pts})
+
+
 # set of pieces which cause lines to flip if player t (str) places a piece at pt ((float,float)) given pieces pcs ([ReversiPiece])
 # THE RULE: a piece at P is a "pivot" for your move M if
 #               1. P is on your team
 #               2. there's at least one piece, and only pieces on the opposite team, intersecting the line segment PM
 #               3. the line segment PM is "filled" in the following sense:
 #                   there is nowhere you could place another piece so it intersects PM but doesn't intersect any existing pieces
-# this takes too long for more than ~20 pieces
+# this is slow for more than ~30 pieces
 # 1 and 2 are checked by the first three clauses of the if (before the lambda)
 # 3 is the hard one. To check it, we try a particular set of potential placements such that (I've convinced myself) if none of them work, then nothing does
 #   these potential placements are:
 #       3.1. tangent to one existing piece in a particular direction (on_line_tangents). each piece close to the line gives 2 of these
 #               the direction is parallel to the line if the piece intersects it, and perpendicular otherwise
-#       3.2. tangent to two existing pieces (double_tangents). each pair of existing pieces gives 0 or 2 of these depending on their distance
-# this takes O(n^3) time for n pieces, since we get at most 2 potential placements from double_tangents for each pair of pieces,
-#   and check whether each potential placement intersects each piece
+#       3.2. tangent to two existing pieces (safe_tangents)
 # we save some time by only considering pieces close enough to the line to matter (i.e. within 3*piece_rad)
 # it's slowest when finding real pivots, since it has to check every potential location (and can be lazy when it fails to find a pivot)
-# I don't know of a way to significantly reduce the number of attempted locations
 # there are pathological cases meaning you can't just look at pieces on the line or something clever like that
-# some testing confirms that the slow part is checking double tangencies (3.1). that should be O(n^3), and 3.2 should be O(n^2)
-# I feel like people say "quadtree" to solve similar problems, but I don't really know how to use those and it sounds like a lot of work
-# I could try spatially binning pieces (have a separate list for each 2x2 square, say) and only looking at the pieces in the nearby bins
-pivots = lambda pcs, t, pt: {
-    pv for pv in pcs 
-        if pv.team==t # 1. pv is on right team
-            and any(in_path(pt, (pv.x,pv.y), (p.x,p.y)) for p in pcs) # 2. there's a piece on the line
-            and all(p.team !=t or not in_path(pt, (pv.x,pv.y), (p.x,p.y)) for p in pcs) # 2. there are only opposite-team pieces
-            and (lambda closePieces: # 3. you can't fit another piece on the line without overlap: try some potential locations
+# s_t is the result of safe_tangents
+pivots = lambda pcs, t, pt: (lambda s_t:[
+        (pv,flipped) for pv,flipped in ((pv,[pc for pc in pcs if in_path(pt, (pv.x,pv.y), (pc.x,pc.y))]) for pv in pcs)
+            if pv.team==t # 1. pv is on right team
+                and flipped # 2. there's a piece on the line
+                and all(p.team !=t for p in flipped) # 2. there are only opposite-team pieces
+                # 3. you can't fit another piece on the line without overlap: try some potential locations
                     # 3.1. tangent to one piece, in the direction (parrallel|perpendicular) to the line
-                    all(not in_path(pt, (pv.x,pv.y), p) or any(overlap(p,(q.x,q.y)) for q in closePieces) or overlap(p,pt) for p1 in closePieces for p in on_line_tangents(pt, (pv.x,pv.y), (p1.x,p1.y)))
+                and (lambda closePieces: 
+                        all(not in_path(pt, (pv.x,pv.y), p) or any(overlap(p,(q.x,q.y)) for q in closePieces) or overlap(p,pt) for p1 in closePieces for p in on_line_tangents(pt, (pv.x,pv.y), (p1.x,p1.y)))
+                    ) ({p for p in pcs if dist_to_line(pt, (pv.x,pv.y), (p.x,p.y)) < 3*piece_rad}) # save time by only looking at pieces nearish the line
                     # 3.2. tangent to two pieces
-                and all(not in_path(pt, (pv.x,pv.y), p) or any(overlap(p,(q.x,q.y)) for q in closePieces) or overlap(p,pt) for p1 in closePieces for p2 in closePieces for p in double_tangents((p1.x,p1.y),(p2.x,p2.y)))
-            ) ([p for p in pcs if dist_to_line(pt, (pv.x,pv.y), (p.x,p.y)) < 3*piece_rad]) # save time by only looking at pieces nearish the line
-}
+                and not any(in_path(pt, (pv.x,pv.y), p) for p in s_t)
+    ]) (safe_tangents([(p.x,p.y) for p in pcs]+[pt]))
 
 printr = lambda x: (print(x),x)[1]
 
@@ -149,18 +156,20 @@ def attemptMove(game, pos):
 
 def updateMove(game, pos):
     game.blockers = {p for p in game.layers[Layers.PIECES] if overlap(pos, (p.x,p.y))}
-    game.pivots = pivots(game.layers[Layers.PIECES], game.turn, pos)
-    game.flippers = {p for p in game.layers[Layers.PIECES] if any(in_path(pos, (pv.x,pv.y), (p.x,p.y)) for pv in game.pivots)}
+    game.pivots, flipped = (lambda t: zip(*t) if t else ([],[]))(pivots(game.layers[Layers.PIECES], game.turn, pos))
+    game.flippers = {p for ps in flipped for p in ps}
     game.mousePos = pos
 
 # test speed:
 if 0:
     import random
-    spread = 5
+    spread = 10
     count = 30
     [game.makePiece(random.choice(['WHITE','BLACK']),random.random()*spread,random.random()*spread) for _ in range(count)]
 
 game.click[1] = lambda e: attemptMove(game, game.point(*e.pos))
-game.drag[-1] = lambda e: updateMove(game, game.point(*e.pos))
+# this says if there are more MOUSEMOTION events in the queue, go through them only running the right click (pan) handler, and skip computing pivots and flippers
+game.drag[-1] = lambda e: [game.drag[2](ev) for ev in pygame.event.get(pygame.MOUSEMOTION) if ev.buttons[2]] if pygame.event.peek(pygame.MOUSEMOTION) else updateMove(game, game.point(*e.pos))
 
-while 1: game.update()
+while 1: 
+    game.update()
