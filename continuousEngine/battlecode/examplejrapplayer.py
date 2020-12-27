b53dfb21d474f3dbbb0dcd44b68feb660db70c94
dylan <dylanhen@mit.edu>
2020-12-27 12:57:33 -0800

    set up jrap for battlecode

---
+++ continuousEngine/battlecode/examplejrapplayer.py
@@ -0,0 +1,14 @@
+import random
+from continuousEngine import *
+from continuousEngine.games.go import *
+from continuousEngine.battlecode.player import *
+
+class Player(PlayerTemplate):
+    def make_move(self):
+        while 1:
+            pos = Point(*(random.uniform(-board_rad,board_rad) for _ in range(2)))
+
+            self.game.updateMove(pos)
+
+            if self.game.valid_move:
+                return {"player":self.team, "location":pos.coords}
