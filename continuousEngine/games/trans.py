c907a90d9551750a52539d797c2559bfef2c311c
dylan <dylanhen@mit.edu>
2021-08-14 19:17:53 -0400

    random number of goals

--- continuousEngine/games/trans.py
+++ continuousEngine/games/trans.py
@@ -3,14 +3,11 @@ import random
 
 # todo
 # better scoring: either compute steiner tree or interface for players to build it
-# choose distributions (number, size, position, randomness)
-#   3x3 grid of squares
-#   no regions; constrain minimum spanning tree to an interval
 # tweak colors / sizes / etc.
 # example battlecode player
 
 class Layers:
-    DISTRIBUTION    = 1
+    CENTER_MARK    = 1
     GOAL            = 2
     RANGE_GUIDE     = 3
     START_PEG       = 4
@@ -27,16 +24,18 @@ class Colors:
     NEW_TREE        = (100, 100, 100)
     NEW_EDGE        = (150, 150, 150)
     BACKGROUND      = (192, 230, 168)
-    DISTRIBUTION    = (200,200,200)
+    CENTER_MARK    = (200,200,200)
     SCORE_TICK      = {0: (255,0,0), 1:(0,0,0)}
     START_PEG       = {'red':(255,50,50), 'blue':(50,50,255), 'green':(0,200,0), 'yellow':(255,255,0), 'brown':(138,85,15), 'white':(235,235,235)}
     GOAL_BORDER     = {'red':(255,50,50), 'blue':(50,50,255), 'green':(0,200,0), 'yellow':(255,255,0), 'brown':(138,85,15), 'white':(235,235,235)}
     RANGE_GUIDE     = {'red':(255,150,150), 'blue':(150,150,255), 'green':(150,255,150), 'yellow':(255,255,150), 'brown':(180, 150, 100), 'white':(225,225,225)}
     SCORE_MARKER    = {'red':(255,50,50), 'blue':(50,50,255), 'green':(0,225,0), 'yellow':(255,255,0), 'brown':(138,85,15), 'white':(255,255,255)}
-    GOAL_FILL       = {'pink':(227,148,164), 'cyan':(55,175,222), 'white':(225,225,225)}
 
 class Constants:
-    INITIAL_SCORE   = 10
+    GOAL_NUM        = 3
+    TARGET_MST      = 15
+    POISSON_STEP    = 100
+    EXPECTED_GOALS  = 4.5
 
     # pixels
     SNAP_TARGET     = 5
@@ -48,6 +47,9 @@ class Constants:
 
     # in-game units
     TURN_DISTANCE   = 1
+    INITIAL_SCORE   = 10
+    X_VARIANCE      = 4 # before scaling to TARGET_MST
+    Y_VARIANCE      = 3
 
 
 class TransTree(Renderable):
@@ -116,33 +118,6 @@ class TransScore(Renderable):
             # pygame.draw.circle(self.game.screen, Colors.SCORE_MARKER[t], (scoretopixel(self.game.score[t]), top + (i+1.5)*Constants.SCORE_VSEP), 10)
             self.game.screen.blit(self.markers[t], (scoretopixel(self.game.score[t]), top + i*Constants.SCORE_VSEP + self.marker_height/2))
 
-# distributions for sampling goals
-# should be a Renderable with a sample() function that returns a Point
-
-class UniformDiskDist(Disk):
-    def __init__(self, game, color_name, coords, r):
-        super().__init__(game, Layers.DISTRIBUTION, Colors.DISTRIBUTION[color_name], Point(*coords), r)
-        self.color_name, *_ = self.args = (color_name, coords, r)
-        self.type = 'UniformDiskDist'
-    def sample(self):
-        while (p := Point(random.uniform(-1,1), random.uniform(-1,1))) >> Point(0,0) > 1: pass
-        return self.loc + p*self.r
-
-class UniformRectangleDist(Rectangle):
-    def __init__(self, game, color_name, coords, dx, dy):
-        super().__init__(game, Layers.DISTRIBUTION, Colors.DISTRIBUTION[color_name], Point(*coords), dx, dy)
-        self.color_name, *_ = self.args = (color_name, coords, dx, dy)
-        self.type = 'UniformRectangleDist'
-    def sample(self):
-        return self.loc + Point(random.uniform(0,self.dx), random.uniform(0,self.dy))
-
-
-DISTRIBUTION_TYPE = {
-    'UniformDiskDist' : UniformDiskDist,
-    'UniformRectangleDist' : UniformRectangleDist,
-}
-
-
 # steps:
 #     start_tree: you don't have a tree yet and are placing your start peg
 #     start_edge: you have a tree, and are picking a point on it to start an edge
@@ -159,8 +134,8 @@ class Trans(Game):
         self.teams = self.possible_teams[:teams]
         super().__init__(backgroundColor=Colors.BACKGROUND, name='continuous trans', **kwargs)
 
-        Segment(self, Layers.DISTRIBUTION, Colors.DISTRIBUTION, Point(-.5,-.5), Point(.5,.5))
-        Segment(self, Layers.DISTRIBUTION, Colors.DISTRIBUTION, Point(.5,-.5), Point(-.5,.5))
+        Segment(self, Layers.CENTER_MARK, Colors.CENTER_MARK, Point(-.5,-.5), Point(.5,.5))
+        Segment(self, Layers.CENTER_MARK, Colors.CENTER_MARK, Point(.5,-.5), Point(-.5,.5))
         
         # edges drawn this turn
         self.new_tree = TransTree(self, layer=Layers.AUX_TREE, color=Colors.NEW_TREE)
@@ -194,8 +169,9 @@ class Trans(Game):
         score = score or {t:Constants.INITIAL_SCORE for t in self.teams}
         goals = []
         for t in self.teams:
-            points = [Point(random.gauss(0,1), random.gauss(0,1)) for _ in range(5)]
-            scale = 15 / minimum_spanning_tree_size(points)
+            n = 2+sum(random.random()<1/Constants.POISSON_STEP for _ in range(int(Constants.POISSON_STEP * (Constants.EXPECTED_GOALS - 2))))
+            points = [Point(random.gauss(0,Constants.X_VARIANCE), random.gauss(0,Constants.Y_VARIANCE)) for _ in range(n)]
+            scale = Constants.TARGET_MST / minimum_spanning_tree_size(points)
             center = sum(points, Point(0,0))/len(points)
             goals.extend((t, center + (p-center)*scale) for p in points)
         return (
@@ -203,9 +179,8 @@ class Trans(Game):
             score,
             [],
             {},
-            #[(dist.type, dist.args) for dist in dists],
             goals,
-            7,
+            10,
         )
 
     possible_teams = ['red', 'blue', 'green', 'yellow', 'brown', 'white']
