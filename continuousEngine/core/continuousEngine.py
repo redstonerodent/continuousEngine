5ba82d7a8afe1fac880be13c20d1d359c8a23445
dylan <dylanhen@mit.edu>
2023-09-05 01:52:45 -0400

    removed useless unknown input messages

--- continuousEngine/core/continuousEngine.py
+++ continuousEngine/core/continuousEngine.py
@@ -68,9 +68,9 @@ class Game:
 
         self.handlers = {
             pygame.QUIT: lambda e: sys.exit(),
-            pygame.MOUSEBUTTONDOWN: lambda e: self.click[e.button](e) if e.button in self.click else print('unknown click:',e.button),
-            pygame.MOUSEMOTION: lambda e: ([self.drag[k](e) if k in self.drag else print('unknown motion:',k) for k in range(-1,3) if k<0 or e.buttons[k]]),
-            pygame.KEYDOWN: lambda e: self.keyPress[e.key](e) if e.key in self.keyPress else self.numKey((e.key%1073741864-8)%10) if pygame.K_KP_1<=e.key<=pygame.K_KP_0 or pygame.K_0<=e.key<=pygame.K_9 else print('unknown key:',e.key),
+            pygame.MOUSEBUTTONDOWN: lambda e: self.click[e.button](e) if e.button in self.click else None,
+            pygame.MOUSEMOTION: lambda e: ([self.drag[k](e) if k in self.drag else None for k in range(-1,3) if k<0 or e.buttons[k]]),
+            pygame.KEYDOWN: lambda e: self.keyPress[e.key](e) if e.key in self.keyPress else self.numKey((e.key%1073741864-8)%10) if pygame.K_KP_1<=e.key<=pygame.K_KP_0 or pygame.K_0<=e.key<=pygame.K_9 else None,
             pygame.VIDEORESIZE: lambda e: (setattr(self, 'needViewChange', True), setattr(self, 'needResize', True)),
         }
 
